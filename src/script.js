8f6117962ea25927d022afd07f75d68aaa1d1f58
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-09-16 00:27:06 -0300

    initical commit

--- src/script.js
+++ src/script.js
@@ -1,105 +1,149 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import * as dat from "dat.gui";
+import gsap from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const gltfloader = new GLTFLoader();
 
 // Debug
-const gui = new dat.GUI()
+// const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+let tl = gsap.timeline();
 
-// Materials
+// Brain
+gltfloader.load("brain.gltf", (gltf) => {
+	// gltf.scene.scale.set(0, 0, 0);
+	// gltf.scene.rotation.set(0, 0, 0);
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+	scene.add(gltf.scene);
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+	gui.add(gltf.scene.rotation, "x").min(0).max(9);
+	gui.add(gltf.scene.rotation, "y").min(0).max(9);
+	gui.add(gltf.scene.rotation, "z").min(0).max(9);
+
+	// tl.to(gltf.scene.rotation, { y: 5.1, duration: 1 });
+	// tl.to(gltf.scene.scale, { x: 0.2, y: 0.2, z: 0.2, duration: 1 }, "-=1");
+});
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// const pointLight = new THREE.PointLight(0xeb9295, 0.9);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+// scene.add(pointLight);
+
+const lights = [];
+const lightHelpers = [];
+
+const lightValues = [
+	{ colour: 0xeb9295, intensity: 1, dist: 12, x: 1, y: 0, z: 8 },
+	{ colour: 0xeb9295, intensity: 1, dist: 12, x: -2, y: 1, z: -10 },
+	{ colour: 0xbb534d, intensity: 5, dist: 10, x: 2, y: 10, z: 1 },
+	{ colour: 0xbb534d, intensity: 3, dist: 12, x: 0, y: -10, z: -1 },
+	{ colour: 0xbb534d, intensity: 5, dist: 12, x: 10, y: 3, z: 0 },
+	{ colour: 0xbb534d, intensity: 5, dist: 12, x: -10, y: -1, z: 0 },
+];
+
+for (let i = 0; i < lightValues.length; i++) {
+	lights[i] = new THREE.PointLight(
+		lightValues[i]["colour"],
+		lightValues[i]["intensity"],
+		lightValues[i]["dist"]
+	);
+	lights[i].position.set(
+		lightValues[i]["x"],
+		lightValues[i]["y"],
+		lightValues[i]["z"]
+	);
+	scene.add(lights[i]);
+
+	scene.add(lightHelpers[i]);
+}
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.6,
+	1200
+);
+camera.position.z = -8;
+scene.add(camera);
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+	canvas: canvas,
+	alpha: true,
+});
+renderer.setClearColor("#181818");
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Trackball Controls for Camera
+// const controls = new TrackballControls(camera, renderer.domElement);
+// controls.rotateSpeed = 2;
+// controls.dynamicDampingFactor = 0.15;
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
+	// Update objects
+	// gltfloader.rotation.y = 0.5 * elapsedTime;
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+	// Update Orbital Controls
+	// controls.update();
 
-    // Update Orbital Controls
-    // controls.update()
+	// Render
+	renderer.render(scene, camera);
 
-    // Render
-    renderer.render(scene, camera)
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
+};
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
